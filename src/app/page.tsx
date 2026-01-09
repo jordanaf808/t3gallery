895cf31c1184a323a643e449bbcd2b45654cd4bb
Jordan Aceves-Foster <jordanaf808@gmail.com>
2026-01-09 15:42:10 -0800

    move Images into its own component and within auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,39 @@
 // import Link from "next/link";
 // import { headers } from "next/headers";
 // import Image from "next/image";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {(!images || images.length < 1) && <h1>No images Available...</h1>}
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48">
+          <img src={image.url} width={400} height={300} alt={image.name} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {(!images || images.length < 1) && <h1>No images Available...</h1>}
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} width={400} height={300} alt={image.name} />
-          </div>
-        ))}
-      </div>
-      Hello (Gallery in progress...)
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In Above...
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
