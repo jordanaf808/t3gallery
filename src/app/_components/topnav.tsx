4ae33a0bf76ab4719d68cf6d447c7ebea243eab2
Jordan Aceves-Foster <jordanaf808@gmail.com>
2026-01-09 12:52:28 -0800

    move topnav

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,29 @@
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>
+        <SignedOut>
+          <SignInButton />
+          <SignUpButton>
+            <button className="h-10 cursor-pointer rounded-full bg-[#6c47ff] px-4 text-sm font-medium text-white sm:h-12 sm:px-5 sm:text-base">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default TopNav;
